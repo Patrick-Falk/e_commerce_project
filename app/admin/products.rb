495028c05940512a90a42abc4fba1c7c1cf5a362
Patrick <pfalk54@academic.rrc.ca>
2017-11-11 15:56:08 +0000

    Renamed photos to follow snake case convention.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Product do
 
-  permit_params :name, :description, :stock_quantity, :status, :category_id
+  permit_params :name, :description, :stock_quantity, :status, :category_id, :image
 
 end
 
