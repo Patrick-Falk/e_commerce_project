c754676998ba4784bd4d2ba3c34732af55d74b2c
Patrick <pfalk54@academic.rrc.ca>
2017-11-11 17:50:54 +0000

    added sorting of prouducts to index table

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,21 @@
 class ProductsController < ApplicationController
+
+  helper_method :sort_column, :sort_direction
   def index
-    @products = Product.order(params[:sort])
+    @products = Product.order(sort_column + " " + sort_direction)
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  private
+
+  def sort_column
+    Product.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
