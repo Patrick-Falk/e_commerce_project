2ce6dffb284dbffafa23ef48aeb7d2cd2500a1d5
Patrick <pfalk54@academic.rrc.ca>
2017-12-06 18:53:12 +0000

    Added a page in active admin for Order Summaries.

---
+++ app/admin/order_summary.rb
@@ -0,0 +1,8 @@
+ActiveAdmin.register_page "Orders Summary" do
+  menu label: "Summary of Orders"
+  content do
+    panel "Summary of Orders" do
+      para "Summary of Orders goes here."
+    end
+  end
+end
